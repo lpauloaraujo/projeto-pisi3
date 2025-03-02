607cabd5b0a4287996e5171f860cb3e5cf543465
lpauloaraujo <158222698+lpauloaraujo@users.noreply.github.com>
2025-03-01 23:43:14 -0300

    insights da clusterização atualizados

--- pages/14_clusterizacao.py
+++ pages/14_clusterizacao.py
@@ -95,7 +95,7 @@ if tipo_clusterizacao == "KModes":
 
     st.plotly_chart(fig)
 
-    st.text("Texto explicativo sobre a porcentagem de filmes que lucraram por cluster")
+    st.text("Podemos observar que essa é a sequência do cluster mais sucrativo pro menos: cluster 1 (ação e subgêneros), cluster 0 (comédia e subgêneros), cluster 2 (terror/suspende e subgêneros), cluster 3 (drama e subgêneros).")
 
 else:
 
@@ -122,7 +122,7 @@ else:
 
     st.plotly_chart(fig)
 
-    st.text("Texto explicativo sobre a porcentagem de filmes que lucraram por cluster")
+    st.text("Podemos observar a porcentagem de lucro extrema do cluster 1 (outliers) e, mesmo o cluster 0 não possuindo outliers, podemos perceber")
 
 # Contando quantos registros pertencem a cada combinação de clusters
 heatmap_data = dataset.groupby(['cluster_kmodes', 'cluster_kmeans']).size().reset_index(name='Contagem')
@@ -159,7 +159,7 @@ fig.update_layout(
 
 st.plotly_chart(fig)
 
-st.text("Texto explicativo sobre o heatmap")
+st.text("O Heatmap demonstra a relação na distribuição dos registros entre os clusters categóricos e numéricos. Podemos observar, por exemplo, que a maioria dos filmes com lucratividade muito alta (filmes na posição 1 do eixo X) fazem parte do cluster 1 categórico (filmes de ação e subgêneros).")
 
 contagem_lucro_regioes['categoria'] = contagem_lucro_regioes['cluster_kmodes'].astype(str) + '-' + contagem_lucro_regioes['cluster_kmeans'].astype(str)
 
@@ -178,7 +178,7 @@ fig.update_traces(texttemplate='%{text:.1%}', hovertemplate='%{y:.1%}')
 
 st.plotly_chart(fig)
 
-st.text("Texto explicativo sobre a porcentagem de lucro por região no heatmap")
+st.text("Podemos observar que as regiões que fazem parte do cluster 1 numérico possuem, em média, lucratividades extremamente altas (acima de 90%), devido ao algoritmo ter selecionado filmes que foram muito lucrativos para esse cluster. As outras regiões, embora não tão altas, também possuem boas porcentagens de lucro. Isso é devido ao fato de estarmos trabalhando apenas com filmes que possuíram um grande orçamento.")
 
 if tipo_clusterizacao == "KModes":
     cluster_selecionado = st.selectbox("Selecione o Cluster", ["0", "1", "2", "3"])
@@ -186,54 +186,54 @@ if tipo_clusterizacao == "KModes":
         #Gêneros mais presentes no cluster 0
         fig = grafico_barras('0', cluster0kmodes, 'genres', 5)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os gêneros mais presentes no cluster 0")
+        st.text("O algoritmo K-Modes selecionou para o cluster 0 filmes de comédia e seus subgêneros.")
         #Línguas mais faladas no cluster 0
         fig = grafico_barras('0', cluster0kmodes, 'spoken_languages', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre as línguas mais faladas no cluster 0")
+        st.text("O inglês é a língua mais falada no cluster, as outras seguem equilibradas.")
         #Páises mais presentes no cluster 0
         fig = grafico_barras('0', cluster0kmodes, 'production_countries', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os países mais presentes no cluster 0")
+        st.text("A maioria dos filmes presentes no cluster possuem o país Estados Unidos da América em suas produções.")
     elif cluster_selecionado == "1":
         #Gêneros mais presentes no cluster 1
         fig = grafico_barras('1', cluster1kmodes, 'genres', 5)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os gêneros mais presentes no cluster 1")
+        st.text("O algoritmo K-Modes selecionou para o cluster 1 filmes de ação e seus subgêneros.")
         #Línguas mais faladas no cluster 1
         fig = grafico_barras('1', cluster1kmodes, 'spoken_languages', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre as línguas mais faladas no cluster 1")
+        st.text("O inglês é a língua mais falada no cluster, as outras seguem equilibradas.")
         #Páises mais presentes no cluster 1
         fig = grafico_barras('1', cluster1kmodes, 'production_countries', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os países mais presentes no cluster 1")
+        st.text("A maioria dos filmes presentes no cluster possuem o país Estados Unidos da América em suas produções.")
     elif cluster_selecionado == "2":
         #Gêneros mais presentes no cluster 2
         fig = grafico_barras('2', cluster2kmodes, 'genres', 5)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os gêneros mais presentes no cluster 2")
+        st.text("O algoritmo K-Modes selecionou para o cluster 2 filmes de terror/suspense e seus subgêneros.")
         #Línguas mais faladas no cluster 2
         fig = grafico_barras('2', cluster2kmodes, 'spoken_languages', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre as línguas mais faladas no cluster 2")
+        st.text("O inglês é a língua mais falada no cluster, as outras seguem equilibradas.")
         #Páises mais presentes no cluster 2
         fig = grafico_barras('2', cluster2kmodes, 'production_countries', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os países mais presentes no cluster 2")
+        st.text("A maioria dos filmes presentes no cluster possuem o país Estados Unidos da América em suas produções.")
     else:
         #Gêneros mais presentes no cluster 3
         fig = grafico_barras('3', cluster3kmodes, 'genres', 5)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os gêneros mais presentes no cluster 3")
+        st.text("O algoritmo K-Modes selecionou para o cluster 3 filmes de drama e seus subgêneros.")
         #Línguas mais faladas no cluster 3
         fig = grafico_barras('3', cluster3kmodes, 'spoken_languages', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre as línguas mais faladas no cluster 3")
+        st.text("O inglês é a língua mais falada no cluster, as outras seguem equilibradas.")
         #Páises mais presentes no cluster 3
         fig = grafico_barras('3', cluster3kmodes, 'production_countries', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os países mais presentes no cluster 3")
+        st.text("A maioria dos filmes presentes no cluster possuem o país Estados Unidos da América em suas produções.")
 
 else:
     tipo_clusterizacao = "KMeans"
@@ -242,25 +242,25 @@ else:
         #Gêneros mais presentes no cluster 0
         fig = grafico_barras('0', cluster0kmeans, 'genres', 5)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os gêneros mais presentes no cluster 0")
+        st.text("O cluster 0 gerado pelo K-Means é o cluster que não possui apenas filmes que foram altamente lucrativos, então podemos observar através desse gráfico os gêneros menos propensos a terem uma lucratividade extrema.")
         #Línguas mais faladas no cluster 0
         fig = grafico_barras('0', cluster0kmeans, 'spoken_languages', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre as línguas mais faladas no cluster 0")
+        st.text("A maioria dos filmes do cluster são de língua inglêsa.")
         #Páises mais presentes no cluster 0
         fig = grafico_barras('0', cluster0kmeans, 'production_countries', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os países mais presentes no cluster 0")
+        st.text("A maioria dos filmes do cluster possuem os Estados Unidos da América em sua produção.")
     else:
         #Gêneros mais presentes no cluster 1
         fig = grafico_barras('1', cluster1kmeans, 'genres', 5)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os gêneros mais presentes no cluster 1")
+        st.text("O cluster 1 gerado pelo K-Means é o cluster que possui filmes com as mais altas lucratividades, então podemos observar através do gráficos os gêneros mais propensos a fazerem tanto sucesso.")
         #Línguas mais faladas no cluster 1
         fig = grafico_barras('1', cluster1kmeans, 'spoken_languages', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre as línguas mais faladas no cluster 1")
+        st.text("A maioria dos filmes do cluster são de língua inglêsa.")
         #Páises mais presentes no cluster 1
         fig = grafico_barras('1', cluster1kmeans, 'production_countries', 8)
         st.plotly_chart(fig)
-        st.text("Texto explicativo sobre os países mais presentes no cluster 1")
+        st.text("A maioria dos filmes do cluster possuem os Estados Unidos da América em sua produção.")
